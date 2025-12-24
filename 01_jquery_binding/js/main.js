cea7208020582136cae05b844f820eddc0de6766
kang <ehkang80@gmail.com>
2025-12-24 15:34:39 +0900

    Add files via upload
    Signed-off-by: kang <ehkang80@gmail.com>

---
+++ 01_jquery_binding/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+// 코드작성
+// 버튼을 click 이벤트하면 div 박스의 내부 색상을 변경
+
+
+// 버튼을 클릭하면
+$('button').click(function(){
+    // div박스의 내부 색상을 변경
+    $("div").css({"background":"red"})
+})
+
+})
