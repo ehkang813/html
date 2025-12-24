5a92ca616d0cec1f757bb6cb35ac9400399e4c1d
kang <ehkang80@gmail.com>
2025-12-24 16:58:47 +0900

    Add files via upload
    Signed-off-by: kang <ehkang80@gmail.com>

--- 02_css/js/script2.js
+++ 02_css/js/script2.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    $("ul").css({"display":"flex","gap":20,"margin":"9px 0px","padding": 0})
+    $("ul").css({"display":"flex","justify-content": "center","margin": "0 auto","gap":20})
     $("li").css({"list-style":"none"})
     $(".box1").css({"width":200,"height":200,"border":"1px solid #000","background":"red"})
     $(".box2").css({"width":200,"height":200,"border":"1px solid #000","background":"orange"})
