6c449608015d5ed9bc2ae49b62315fcc7491e5d2
kang <ehkang80@gmail.com>
2025-12-29 12:44:56 +0900

    Add files via upload
    Signed-off-by: kang <ehkang80@gmail.com>

---
+++ 04_slick_slider/22_asan/js/main.js
@@ -0,0 +1,30 @@
+$(function(){
+    $('.slickslide').slick({
+    // 슬라이드 자동 실행
+    autoplay: true,
+    // 페이비버튼 보이기
+    dots:true,
+    // 자동실행 시간 조절(기본값3000)
+    autoplaySpeed: 2000,
+
+    // 좌우버튼
+    arrows: true,
+
+    // 슬라이드를 페이드효과
+    // fade:true,
+
+    // 슬라이드 효과를 상하로할때
+    // vertical:true,
+
+    // 반복 설정
+    infinite: true,
+
+    // 마우스 가져가면 멈추기
+    pauseOnHover:true,
+
+    //스크롤 페이지 건너띄기 
+    // slidesToScroll:2
+    });
+
+
+});
